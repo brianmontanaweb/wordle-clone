7632c88fc1107ca920cdde8a7e42920f6032b013
Brian Montana <brian.montana@MacBook-Pro.local>
2023-01-23 14:05:12 -0600

    Finishing up the basics for Wordle

--- src/routes/start/+page.svelte
+++ src/routes/start/+page.svelte
@@ -2,9 +2,14 @@
 	import Board from 'src/shared/components/board.svelte';
 	import CharItem from 'src/shared/components/charItem.svelte';
 	import LineItem from 'src/shared/components/lineItem.svelte';
-	import { WORD_LENGTH } from 'src/shared/constants/wordle';
+	import { NUMBER_OF_GUESSES, WORD_LENGTH } from 'src/shared/constants/wordle';
 	import { characterKeyPress, getSecretWord, verifySecretWord } from 'src/shared/utils/wordle';
-	import { guessesStore, secretWordStore, wonGameStore } from 'src/stores/wordle';
+	import {
+		currentGuessStore,
+		guessesStore,
+		secretWordStore,
+		wonGameStore
+	} from 'src/stores/wordle';
 	const base = `http://wordle-clone.com/api`;
 
 	async function fetchSecretWord() {
@@ -12,8 +17,8 @@
 			const res = await fetch(`${base}/wordle`);
 			const data = await res.json();
 			if (data) {
-                const secretWord = getSecretWord(data);
-                secretWordStore.update(() => secretWord);
+				const secretWord = getSecretWord(data);
+				secretWordStore.update(() => secretWord);
 				return secretWord;
 			} else {
 				throw new Error('No data present');
@@ -23,19 +28,26 @@
 		}
 	}
 
-	let guesses;
+	let secretWordStart = fetchSecretWord();
+
+	let guesses = new Array(NUMBER_OF_GUESSES).fill(null);
 	guessesStore.subscribe((value) => {
 		guesses = value;
 	});
-    let winner;
-    wonGameStore.subscribe(value => {
-        winner = value;
-    });
-	let secretWordStart = fetchSecretWord();
-    let secretWord;
-    secretWordStore.subscribe(value => {
-        secretWord = value;
-    })
+	let winner = false;
+	wonGameStore.subscribe((value) => {
+		winner = value;
+	});
+	let secretWord = '';
+	secretWordStore.subscribe((value) => {
+		secretWord = value;
+	});
+	let currentGuess = '';
+	currentGuessStore.subscribe((value) => {
+		currentGuess = value;
+	});
+    $: currentGuessIndex = guesses?.findIndex((guess) => guess == null);
+    $: currentGuessChars = currentGuess;
 </script>
 
 <svelte:head>
@@ -47,37 +59,40 @@
 <h1>Start!</h1>
 
 <Board>
-    {#if winner}
-        <h3>YOU WON!</h3>
-        <h3>{secretWord}</h3>
-    {:else}
-	{#await secretWordStart}
-		<h3>loading</h3>
-	{:then data}
-		{#if guesses.length}
-			{#each guesses as word}
-				{#if word && data}
-					<LineItem>
-						{#each verifySecretWord(word, data) as charState, idx}
-							<CharItem {charState}>{word[idx]}</CharItem>
-						{/each}
-					</LineItem>
-				{:else}
-					<LineItem>
-						{#each ''.padEnd(WORD_LENGTH) as char}
-							<CharItem charState={null}>{char}</CharItem>
-						{/each}
-					</LineItem>
-				{/if}
-			{/each}
-		{/if}
-	{/await}
-    {/if}
+	{#if winner}
+		<h3>YOU WON!</h3>
+		<h3>{secretWord}</h3>
+	{:else}
+		{#await secretWordStart}
+			<h3>loading</h3>
+		{:then data}
+			{#if guesses.length}
+				{#each guesses as word, idx}
+					{#if word && data}
+						<LineItem>
+							{#each verifySecretWord(word, data) as charState, jdx}
+								<CharItem {charState}>
+                                    {word[jdx]}
+                                </CharItem>
+							{/each}
+						</LineItem>
+					{:else}
+						<LineItem>
+							{#each ''.padEnd(WORD_LENGTH) as char, jdx}
+								<CharItem charState={null}>{idx === currentGuessIndex && currentGuessChars[jdx] ? currentGuessChars[jdx] : ''}</CharItem>
+							{/each}
+						</LineItem>
+					{/if}
+				{/each}
+			{/if}
+		{/await}
+	{/if}
 </Board>
 
 <style>
-	h1, h3 {
+	h1,
+	h3 {
 		text-align: center;
-        text-transform: uppercase;
+		text-transform: uppercase;
 	}
 </style>

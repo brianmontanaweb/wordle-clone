ee348c73b95d859a2489697f3ec932e1366c0a94
Brian Montana <brian.montana@macbook-pro.lan>
2023-01-24 16:36:22 -0600

    Fixing MSW setup

--- src/mocks/index.js
+++
@@ -1,9 +0,0 @@
-async function inject() {
-	const { worker } = await import('./browser.js');
-	// For live development, I disabled all warnings
-	// for requests that are not mocked. Change how
-	// you think it best fits your project.
-	return worker.start()
-}
-
-inject(); 
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	base: '/wordle-clone/'
+	}
 };
 
 export default config;
